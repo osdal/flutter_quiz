8703a0569dfda7b1a95b61a3919d05eedba462f7
Дмитрий <osdal67@gmail.com>
2022-10-26 09:11:27 +0300

    final

--- lib/result.dart
+++ lib/result.dart
@@ -24,6 +24,7 @@ class Result extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           resultPhrase,
